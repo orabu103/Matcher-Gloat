2d1f6f8fa87e6b61a02595404bc598efd0625e87
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2021-12-18 15:19:14 +0200

    add CandidateFinder second option add requirments file

--- matcher/views.py
+++ matcher/views.py
@@ -1,65 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Count
 from .models import Skill , Job , Candidate , Title
 
 # Create your views here.
 
+
 def Home(request):
       return render(request , 'home/home.html')
 
+# The function gets a Job ,Looking for the candidates with the same title
+# Goes through all the skills of the job and checks which candidate has more suitable skills
 def CandidateFinder(request):
-    candidates = 'A'
+    candidates = ""
     try:
         id = request.GET['job']
         job = Job.objects.get(id = id)
-        job_skills = job.skill.all()
         candidates = Candidate.objects.filter(title = job.title)
-        count = [i.id for i in job_skills]
-        print(count)
-        print(job)
-        print(job_skills)
+        candidates = candidates.filter(skill__in = job.skill.all()).annotate(sum=Count('id')).order_by('-sum')
         print(candidates)
         
-        for candidate in candidates:
-            print(candidate.skill.all())
-            counter = 0
-            countin = [i.id for i in candidate.skill.all()]
-            for i in countin:
-                print(i)
-                if(i in count):
-                    counter+=1
-                if(i > count[len(count)-1]):
-                    break
-            (countin)
-            print(counter)
-     
     except Exception as e:
-        print(e)
-    jobs = Job.objects.all()
-    return render(request , 'candidateFinder/candidateFinder.html' , {'jobs':jobs , 'candidate' :candidates})
+        err = e
     
- 
+    jobs = Job.objects.all()
+    return render(request , 'candidateFinder/candidateFinder.html' , {'jobs':jobs , 'candidates' :candidates} )
+    
+
+# This function adds skills to the table Skill
 def Skills(request):
     try:
         name = request.POST['name']
         skill = Skill(name = name)
         skill.save()
     except Exception as e:
-        print(e)
+        err = e
     skills = Skill.objects.all()
     return render(request , 'skills/skills.html' , {'skills': skills})
 
+# This function adds titles to the table Title
 def Titles(request):
     try:
         name = request.POST['name']
         title = Title(name = name)
         title.save()
     except Exception as e:
-        print(e)
+        err = e
     titles = Title.objects.all()
     return render(request , 'titles/titles.html' , {'titles': titles})
 
-
+# This function adds jobs to the table Jobs
+# After selecting a job title and required skills
 def Jobs(request):
     try:
         title = request.POST['title']
@@ -70,13 +60,14 @@ def Jobs(request):
             job.skill.add(Skill.objects.get(id = int(i)))  
         print(job.skill.all())
     except Exception as e:
-       print(e)
+       err = e
     jobs = Job.objects.all()
     skills = Skill.objects.all()
     titles = Title.objects.all()
     return render(request , 'job/job.html' , {'jobs': jobs , 'skills': skills , 'titles' : titles})
 
-
+# This function adds candidate to the table Candidates
+# After selecting a candidate title and skills
 def Candidates(request):
     try:
         title = request.POST['title']
@@ -87,7 +78,7 @@ def Candidates(request):
             candidate.skill.add(Skill.objects.get(id = int(i)))  
         print(candidate.skill.all())
     except Exception as e:
-        print(e)
+        err = e
     candidates = Candidate.objects.all()
     skills = Skill.objects.all()
     titles = Title.objects.all()
